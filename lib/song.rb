7f9eb60243c916bd094c9e039f6e84e96a8435a5
Amgad Malik <amgadmalik@yahoo.com>
2019-10-09 19:32:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,7 +51,8 @@ class Song
   def self.new_from_filename(file)
     
     song_array = file.split(" - ")
-    song_second_array = song_array.chomp
+    song_second_array = song_array.chomp(".mp3")
+    
     @name = song_name
     @artist_name = file.split(" - ")[0].to_s
     #binding.pry
